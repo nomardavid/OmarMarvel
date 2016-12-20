685b1ddd69e6c2883496bacd979e7d9f2db3a510
Omar Garcia <ogarcia@labsxd.com>
2016-12-20 19:00:07 -0400

    add last changes

--- dist/styles.5f7f16a82a4f91544984.bundle.js
+++
@@ -1,2 +0,0 @@
-webpackJsonp([2,3],{419:function(n,o){},420:function(n,o){},421:function(n,o){},993:function(n,o,c){c(419),c(420),n.exports=c(421)}},[993]);
-//# sourceMappingURL=styles.5f7f16a82a4f91544984.bundle.map
